ab8ae0a64ebf6d7bdd53b0a6dc49b184367da6f6
Marek Sapota <marek@sapota.org>
2013-12-27 00:51:26 -0800

    Follow symlinks

--- utils/python_clean.mk
+++ utils/python_clean.mk
@@ -1,3 +1,3 @@
 .PHONY: press_clean_python
 press_clean_python:
-	-rm -rf $(shell find . -type d -name __pycache__)
+	-rm -rf $(shell find -L . -type d -name __pycache__)
--- utils/scss_clean.mk
+++ utils/scss_clean.mk
@@ -1,4 +1,4 @@
 .PHONY: press_clean_css
 press_clean_css:
-	-rm -f $(shell find . -type f -name \*.css)
-	-rm -rf $(shell find . -type d -name .sass-cache)
+	-rm -f $(shell find -L . -type f -name \*.css)
+	-rm -rf $(shell find -L . -type d -name .sass-cache)
